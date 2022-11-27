91697bd1faad71dc75729a746e09923378ecaac4
dev.lauer <dev.lauer@elnarion.de>
2022-11-27 17:08:57 +0100

    extract test user/pwd to constant

--- jsf/src/test/java/com/github/kaiwinter/testsupport/arquillian/WildflyDockerExtension.java
+++ jsf/src/test/java/com/github/kaiwinter/testsupport/arquillian/WildflyDockerExtension.java
@@ -35,6 +35,8 @@ public final class WildflyDockerExtension implements LoadableExtension {
 	 */
 	public static final class LoadContainerConfiguration {
 
+		private static final String WILDFLY_USER = "admin";
+		private static final String WILDFLY_PWD = "Admin#70365";
 		private static final int WILDFLY_HTTP_PORT = 8080;
 		private static final int WILDFLY_MANAGEMENT_PORT = 9990;
 
@@ -50,7 +52,7 @@ public final class WildflyDockerExtension implements LoadableExtension {
 			GenericContainer<?> dockerContainer = new GenericContainer<>(
 					new ImageFromDockerfile()
 							.withDockerfileFromBuilder(builder -> builder.from("quay.io/wildfly/wildfly:26.1.2.Final")
-									.user("jboss").run("/opt/jboss/wildfly/bin/add-user.sh admin Admin#70365 --silent")
+									.user("jboss").run("/opt/jboss/wildfly/bin/add-user.sh "+WILDFLY_USER+" "+WILDFLY_PWD+" --silent")
 									.cmd("/opt/jboss/wildfly/bin/standalone.sh", "-b", "0.0.0.0", "-bmanagement",
 											"0.0.0.0")
 									.build()))
@@ -71,8 +73,8 @@ public final class WildflyDockerExtension implements LoadableExtension {
 			containerConfiguration.property(ContainerConfiguration.MANAGEMENT_ADDRESS_KEY, containerIpAddress);
 			containerConfiguration.property(ContainerConfiguration.MANAGEMENT_PORT_KEY,
 					String.valueOf(wildflyManagementPort));
-			containerConfiguration.property(ContainerConfiguration.USERNAME_KEY, "admin");
-			containerConfiguration.property(ContainerConfiguration.PASSWORD_KEY, "Admin#70365");
+			containerConfiguration.property(ContainerConfiguration.USERNAME_KEY, WILDFLY_USER);
+			containerConfiguration.property(ContainerConfiguration.PASSWORD_KEY, WILDFLY_PWD);
 
 			ProtocolDef protocolConfiguration = arquillianContainer
 					.getProtocolConfiguration(new ProtocolDescription(ServletProtocolDefinition.NAME));
